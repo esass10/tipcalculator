5c2f5f75d7faa1cd984a31fc3abc10820fa62e85
Elliot Sasson <18esasson@athenian.org>
2019-03-30 12:49:30 -0700

    Initial Commit

---
+++ tipcalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipcalculator
+//
+//  Created by Elliot Sasson on 3/30/19.
+//  Copyright © 2019 elliotsasson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
